23e94449756068cc86195f647bbfdbdd2f9f4a29
vauthrib <vauthrib@gmail.com>
2026-04-02 18:50:27 +0000

    V2.59 - Double A5: median values between V2.57 and V2.58

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "SRGA V2.58 - Gestion de Facturation",
+  title: "SRGA V2.59 - Gestion de Facturation",
   description: "Application de gestion de facturation",
 };
 
